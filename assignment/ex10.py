320b21ca3cdfcbf4d62b2d5a77acde45b6c53c73
NishantRajora <nishantrajora100@gmail.com>
2024-11-07 21:13:03 +0530

    upgrade

--- assignment/ex10.py
+++ assignment/ex10.py
@@ -4,9 +4,9 @@ import pandas as pd
 Car_Price = {'Company': ['Toyota', 'Honda', 'BMW', 'Audi'], 'Price': [23845, 17995, 135925, 71400]}
 car_Horsepower = {'Company': ['Toyota', 'Honda', 'BMW', 'Audi'], 'horsepower': [141, 80, 182, 160]}
 
-# Create dataframes from the dictionaries
 df_price = pd.DataFrame(Car_Price)
 df_horsepower = pd.DataFrame(car_Horsepower)
+
 
 # Merge the two dataframes on the 'Company' column
 df_merged = pd.merge(df_price, df_horsepower, on='Company')
